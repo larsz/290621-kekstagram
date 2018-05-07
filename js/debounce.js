f77d1d50085756168bc4b518d8028c15d77de319
larsz <eugen.lobicov@gmail.com>
2018-05-08 00:22:04 +0300

    Переименовывает параметр, убирает лишний return в сортировке

--- js/debounce.js
+++ js/debounce.js
@@ -5,11 +5,11 @@
   var DEBOUNCE_INTERVAL = 500;
   var lastTimeout;
 
-  window.debounce = function (fun) {
+  window.debounce = function (callback) {
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
     }
-    lastTimeout = window.setTimeout(fun, DEBOUNCE_INTERVAL);
+    lastTimeout = window.setTimeout(callback, DEBOUNCE_INTERVAL);
   };
 
 })();
